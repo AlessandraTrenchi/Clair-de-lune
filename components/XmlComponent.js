d28b9454496613ad0d19ae0ea2760ab8ede05055
AlessandraTrenchi <alessandra.trenchi@studio.unibo.it>
2023-06-22 19:05:53 +0200

    xmlbuilder2
    this is with xmlbuilder2

---
+++ components/XmlComponent.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { create } from 'xmlbuilder2';
+import { XmlWriter } from 'cetei';
+
+const XmlComponent = () => {
+  useEffect(() => {
+    const generateXml = () => {
+       const writer = new XmlWriter();
+       writer.startDocument(); //cetei methods: starts a new xml doc
+       writer.startElement('root'); //starts a new element with the tagName root
+       writer.writeElement('element', 'Contenuto del tuo element'); //builds an element with the specified tagname, and textContent
+       writer.endElement(); //closes the current element
+       writer.endDocument(); //returns the xml representation as a string
+
+       const xmlString = writer.toString();
+       console.log(xmlString);
+      };
+      generateXml(); //will generate a xml document
+    }, []);
+
+  return (
+    <div>
+      {/* Your JSX code for the component */}
+    </div>
+  );
+}; 
+
+
