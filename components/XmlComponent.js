2177e340bb25bdb8028f3b2f9dc8e9dded67a10b
AlessandraTrenchi <alessandra.trenchi@studio.unibo.it>
2023-06-23 15:30:42 +0200

    Update XmlComponent.js

--- components/XmlComponent.js
+++ components/XmlComponent.js
@@ -1,30 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { CETEI } from 'ceteicean';
 
 const XmlComponent = () => {
+  const [xmlString, setXmlString] = useState('');
+
   useEffect(() => {
     const generateXml = async () => {
-        const ceteicean = new CETEI();
-        ceteicean.create('1.0'); // Creates a new XML document with version 1.0
-        ceteicean.open('root'); // Opens a new element with the tag name 'root'
-        ceteicean.open('element'); // Opens a new element with the tag name 'element'
-        ceteicean.text('Contenuto del tuo element'); // Adds text content to the current element
-        ceteicean.close(); // Closes the current element
-        ceteicean.close(); // Closes the 'root' element
-        const xmlString = await ceteicean.saveXML(); // Returns the XML representation as a string
-        console.log(xmlString);
-      };
-  
+      const ceteicean = new CETEI();
+      ceteicean.create('1.0'); // Creates a new XML document with version 1.0
+      ceteicean.open('root'); // Opens a new element with the tag name 'root'
+      ceteicean.open('element'); // Opens a new element with the tag name 'element'
+      ceteicean.text('Contenuto del tuo element'); // Adds text content to the current element
+      ceteicean.close(); // Closes the current element
+      ceteicean.close(); // Closes the 'root' element
+      const generatedXmlString = await ceteicean.saveXML(); // Returns the XML representation as a string
+      setXmlString(generatedXmlString);
+    };
 
-      generateXml(); //will generate a xml document
-    }, []);
+    generateXml();
+  }, []);
 
   return (
     <div>
-      {/* Your JSX code for the component */}
+      <pre>{xmlString}</pre>
     </div>
   );
-}; 
+};
 
 export default XmlComponent;
-
