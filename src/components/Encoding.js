2ac6b1f6870768f9306e346b2dc59538bc63268b
AlessandraTrenchi <alessandra.trenchi@studio.unibo.it>
2023-06-28 15:51:26 +0200

    updated encoding

--- src/components/Encoding.js
+++ src/components/Encoding.js
@@ -3,37 +3,37 @@ import React from 'react';
 const Encoding = () => {
   return (
     <div>
-      <interpGrp type="emotions">
-        <desc>Emotions encoded in the text</desc>
-        <interp xml:id="joy" type="positive">Joy</interp>
-        <interp xml:id="trust" type="positive">Trust</interp>
-        <interp xml:id="anticipation" type="positive">Anticipation</interp>
-        <interp xml:id="sadness" type="negative">Sadness</interp>
-        <interp xml:id="disgust" type="negative">Disgust</interp>
-        <interp xml:id="anger" type="negative">Anger</interp>
-        <interp xml:id="fear" type="negative">Fear</interp>
-        <interp xml:id="surprise" type="negative">Surprise</interp>
-      </interpGrp>
+      <div type="emotions">
+        <p>Emotions encoded in the text</p>
+        <p id="joy" type="positive">Joy</p>
+        <p id="trust" type="positive">Trust</p>
+        <p id="anticipation" type="positive">Anticipation</p>
+        <p id="sadness" type="negative">Sadness</p>
+        <p id="disgust" type="negative">Disgust</p>
+        <p id="anger" type="negative">Anger</p>
+        <p id="fear" type="negative">Fear</p>
+        <p id="surprise" type="negative">Surprise</p>
+      </div>
 
-      <interpGrp type="emotion-causes">
-        <desc>Causes linked to emotions encoded in the text</desc>
-        <interp xml:id="cause-character">
+      <div type="emotion-causes">
+        <p>Causes linked to emotions encoded in the text</p>
+        <p id="cause-character">
           A character that can affect the emotional state of another character.
-        </interp>
-        <interp xml:id="cause-event">
+        </p>
+        <p id="cause-event">
           Event, that can influence a character's state (e.g. someone's death)
-        </interp>
-      </interpGrp>
+        </p>
+      </div>
 
-      <interpGrp type="emotion-effect">
-        <desc>Effects linked to emotions encoded in the text</desc>
-        <interp xml:id="positive-effect" type="positive">
+      <div type="emotion-effect">
+        <p>Effects linked to emotions encoded in the text</p>
+        <p id="positive-effect" type="positive">
           A positive effect to a character
-        </interp>
-        <interp xml:id="negative-effect" type="negative">
+        </p>
+        <p id="negative-effect" type="negative">
           A negative effect to a character
-        </interp>
-      </interpGrp>
+        </p>
+      </div>
     </div>
   );
 };
