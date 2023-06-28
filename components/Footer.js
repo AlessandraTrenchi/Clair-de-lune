36f8686f58bdea2309491e62651a7539331f1232
AlessandraTrenchi <alessandra.trenchi@studio.unibo.it>
2023-06-28 13:29:15 +0200

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <p>&copy; 2023 Alessandra Trenchi and Chloi Papadopoulou</p>
+        <p>Website and encoding developed for the course of <link to="https://www.unibo.it/it/didattica/insegnamenti/insegnamento/2022/467035"><i>Digital Text in the Humanities</i></link> held by Professor <link to="https://www.unibo.it/sitoweb/tiziana.mancinelli2">Tiziana Mancinelli</link></p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
