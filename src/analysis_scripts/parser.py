dfaf88dca5ef61f5b69345fb2117acf577b87fc0
Chloe <chloepap4@gmail.com>
2023-07-02 18:27:23 +0200

    complete stories xml

--- src/analysis_scripts/parser.py
+++ src/analysis_scripts/parser.py
@@ -3,23 +3,7 @@ import os
 import networkx as nx
 import matplotlib.pyplot as plt
 import itertools
-# # Read the XML file
-# with open('text/bijoux.xml', 'r') as file:
-#     xml_data = file.read()
 
-# # Create a Beautiful Soup object with the XML data and the parser
-# soup = BeautifulSoup(xml_data, 'xml')
-
-# # Perform operations on the parsed XML data
-# # For example, access specific tags or attributes
-# title = soup.find('title').text
-# print(f"Title: {title}")
-
-# # Extract data from multiple elements
-# for item in soup.find_all('item'):
-#     name = item.find('name').text
-#     value = item.find('value').text
-#     print(f"Name: {name}, Value: {value}")
 
 from pathlib import Path
 
@@ -34,91 +18,52 @@ with open(path+'/text/bijoux.xml', 'r') as file:
 # # Create a Beautiful Soup object with the XML data and the 'lxml' parser
 moonsoup = BeautifulSoup(xml_data, features='xml')
 
-# #gather the stories which are structured inside divs
-# divs = moon_soup.find_all('div')
-
-# #make list of the stories
-# stories_l = [tag.content for tag in divs]
 
 
-# print(stories_l[0])
-# interp_love = soup.find_all('interp', ana='#love')
-# print(interp_love)
-
-
-# find interp in each paragraph
-# make a network
-# connect to causes????
-
-
-
-# Isolate each paragraph or div
-# create a graph for each div id
-# associate emotions in the div or the paragraphs
-# if the tag contains event -- append with event, same for character
-
-# Find all div elements with type="story"
 story_divs = moonsoup.find_all('div', type='story')
 
-# print(story_divs[0])
-# # Extract information for each story
-# for div in story_divs:
-#     story_id = div['xml:id']
-#     # title = div.find('head').text
-#     paragraphs = div.find_all('p')
 
-#     ana_counts = {}
-#     for p in paragraphs:
-#         interp_tags = p.find_all('interp')
-#         for tag in interp_tags:
-#             ana = tag['ana']
-#             if ana in ana_counts:
-#                 ana_counts[ana] += 1
-#             else:
-#                 ana_counts[ana] = 1
-    
-#     print(f"Story ID: {story_id}")
-#     # print(f"Title: {title}")
-#     print(f"Interp Tag Counts: {ana_counts}")
-#     print()
 
 G=nx.Graph()
-# for div in story_divs:
-#     paragraphs = div.find_all('p')
 
-# # Extract emotions and add edges to the graph
-# emotions = set()
-# for p in paragraphs:
-#     interp_tags = p.find_all('interp')
-#     for tag in interp_tags:
-#         ana = tag['ana']
-#         emotions.add(ana)
+# # Iterate over each story and create a network
+# for story in story_divs:
+#     interps = story.find_all("interp")
 
-# # Add edges between all emotions
-# emotion_pairs = list(itertools.combinations(emotions, 2))
-# G.add_edges_from(emotion_pairs)
+#     emotions = set()
+#     for interp in interps:
+#         ana = interp.get("ana")
+#         emotions.add(ana.split()[0].lstrip("#"))  # Extract the first part of ana
 
-# # Draw the network graph
-# pos = nx.spring_layout(G)
-# nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='gray')
-# plt.show()
-# nx.write_gexf(G,"n1.gexf")
+#         # Check if ana contains #cause-character
+#         if "#cause-character" in ana:
+#             G.add_node("cause-character")
+#             G.add_edge(ana.split()[0].lstrip("#"), "cause-character")
+
+#         # Check if ana contains #cause-event
+#         if "#cause-event" in ana:
+#             G.add_node("cause-event")
+#             G.add_edge(ana.split()[0].lstrip("#"), "cause-event")
+        
+#         else:
+#             G.add_node("cause-internal")
+#             G.add_edge(ana.split()[0].lstrip("#"), "cause-internal")
 
 
+#     # Add nodes for each emotion
+#     for emotion in emotions:
+#         G.add_node(emotion)
 
-# import networkx as nx
-# import matplotlib.pyplot as plt
-# from bs4 import BeautifulSoup
-# import itertools
+#     # Add edges between all emotions
+#     emotion_pairs = list(itertools.combinations(emotions, 2))
+#     G.add_edges_from(emotion_pairs)
 
-# # Parse the XML file
-# soup = BeautifulSoup(xml_data, "lxml")
 
-# # Find all the div elements with type="story"
-# stories = soup.find_all("div", {"type": "story"})
 
 # Iterate over each story and create a network
 for story in story_divs:
+    div_id = story.get("xml:id")  # Get the div ID
+
     interps = story.find_all("interp")
 
     emotions = set()
@@ -128,22 +73,21 @@ for story in story_divs:
 
         # Check if ana contains #cause-character
         if "#cause-character" in ana:
-            G.add_node("cause-character")
+            G.add_node("cause-character", div_id=div_id, type="emotion-cause")  # Add div_id as a node attribute
             G.add_edge(ana.split()[0].lstrip("#"), "cause-character")
 
         # Check if ana contains #cause-event
         if "#cause-event" in ana:
-            G.add_node("cause-event")
+            G.add_node("cause-event", div_id=div_id, type="emotion-cause")  # Add div_id as a node attribute
             G.add_edge(ana.split()[0].lstrip("#"), "cause-event")
         
         else:
-            G.add_node("cause-internal")
+            G.add_node("cause-internal", div_id=div_id, type="emotion-cause")  # Add div_id as a node attribute
             G.add_edge(ana.split()[0].lstrip("#"), "cause-internal")
-
 
     # Add nodes for each emotion
     for emotion in emotions:
-        G.add_node(emotion)
+        G.add_node(emotion, div_id=div_id, type="emotion", name=emotion)  # Add div_id as a node attribute
 
     # Add edges between all emotions
     emotion_pairs = list(itertools.combinations(emotions, 2))
@@ -155,3 +99,59 @@ plt.title("Emotion Network - All Stories")
 plt.show()
 
 nx.write_gexf(G, "net.gexf")
+
+
+# from collections import Counter
+
+# # ...
+
+# # Create a dictionary to store edge weights
+# edge_weights = Counter()
+
+# # Iterate over each story and create a network
+# for story in story_divs:
+#     div_id = story.get("xml:id")  # Get the div ID
+
+#     interps = story.find_all("interp")
+
+#     emotions = set()
+#     for interp in interps:
+#         ana = interp.get("ana")
+#         emotions.add(ana.split()[0].lstrip("#"))  # Extract the first part of ana
+
+#         # Check if ana contains #cause-character
+#         if "#cause-character" in ana:
+#             if div_id is not None:
+#                 G.add_node("cause-character", div_id=div_id, type="emotion-cause")  # Add div_id as a node attribute
+#             G.add_edge(ana.split()[0].lstrip("#"), "cause-character")
+#             edge_weights[(ana.split()[0].lstrip("#"), "cause-character")] += 1  # Increment edge weight
+
+#         # Check if ana contains #cause-event
+#         if "#cause-event" in ana:
+#             if div_id is not None:
+#                 G.add_node("cause-event", div_id=div_id, type="emotion-cause")  # Add div_id as a node attribute
+#             G.add_edge(ana.split()[0].lstrip("#"), "cause-event")
+#             edge_weights[(ana.split()[0].lstrip("#"), "cause-event")] += 1  # Increment edge weight
+
+#         else:
+#             if div_id is not None:
+#                 G.add_node("cause-internal", div_id=div_id, type="emotion-cause")  # Add div_id as a node attribute
+#             G.add_edge(ana.split()[0].lstrip("#"), "cause-internal")
+#             edge_weights[(ana.split()[0].lstrip("#"), "cause-internal")] += 1  # Increment edge weight
+
+#     # Add nodes for each emotion
+#     for emotion in emotions:
+#         if div_id is not None:
+#             G.add_node(emotion, div_id=div_id, type="emotion")  # Add div_id as a node attribute
+
+# # Update edge weights in the graph
+# for edge, weight in edge_weights.items():
+#     G[edge[0]][edge[1]]['weight'] = weight
+
+# Plot the network
+# nx.draw(G, with_labels=True)
+# plt.title("Emotion Network - All Stories")
+# plt.show()
+
+# nx.write_gexf(G, "net.gexf")
+
