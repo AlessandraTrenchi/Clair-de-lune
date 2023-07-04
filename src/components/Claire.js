e3a09c62d889ff07ef1a60c2baa33371ba0df950
AlessandraTrenchi <alessandra.trenchi@studio.unibo.it>
2023-07-04 16:26:30 +0200

    ceteicean

---
+++ src/components/Claire.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import XmlComponent from './XmlComponent';
+
+const Claire = () => {
+  return (
+    <div>
+      {/* Other content */}
+      <XmlComponent />
+    </div>
+  );
+};
+
+export default Claire;
--- src/components/XmlComponent.js
+++ src/components/XmlComponent.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { CETEI } from 'ceteicean';
+import { CeteiCean } from 'cetei-cean';
+import 'cetei-cean/dist/ceteicean.css';
+import bijouxXML from '../text/bijoux.xml';
 
 const XmlComponent = () => {
   const [xmlString, setXmlString] = useState('');
@@ -7,22 +10,29 @@ const XmlComponent = () => {
   useEffect(() => {
     const generateXml = async () => {
       const ceteicean = new CETEI();
-      ceteicean.create('1.0'); // Creates a new XML document with version 1.0
-      ceteicean.open('root'); // Opens a new element with the tag name 'root'
-      ceteicean.open('element'); // Opens a new element with the tag name 'element'
-      ceteicean.text('Contenuto del tuo element'); // Adds text content to the current element
-      ceteicean.close(); // Closes the current element
-      ceteicean.close(); // Closes the 'root' element
-      const generatedXmlString = await ceteicean.saveXML(); // Returns the XML representation as a string
+      ceteicean.load(bijouxXML); // Load the XML file
+      const generatedXmlString = await ceteicean.saveXML();
       setXmlString(generatedXmlString);
     };
 
     generateXml();
   }, []);
 
+  useEffect(() => {
+    const renderAnnotatedText = () => {
+      const ceteiCean = new CeteiCean();
+      const container = document.getElementById('annotated-text-container');
+      ceteiCean.loadFromString(xmlString, container);
+    };
+
+    if (xmlString) {
+      renderAnnotatedText();
+    }
+  }, [xmlString]);
+
   return (
     <div>
-      <pre>{xmlString}</pre>
+      <div id="annotated-text-container" />
     </div>
   );
 };
