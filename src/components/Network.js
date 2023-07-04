d8829c423d87ecd7650470c96f0cf98a8de059c2
Chloe <chloepap4@gmail.com>
2023-07-04 17:00:38 +0200

    documentation network update

--- src/components/Network.js
+++ src/components/Network.js
@@ -3,14 +3,14 @@ import React from 'react';
 const Network = () => {
   return (
     <div className='nett'>
-      <iframe
-        width="800"
-        height="600"
-        src="https://ouestware.gitlab.io/retina/1.0.0-beta.1/#/embed/?url=https%3A%2F%2Fgist.githubusercontent.com%2Fchloeppd%2Fda85a845be35f7f8a69557542b244fe6%2Fraw%2F65774543a6fd55e00e4f782c4b9430efc704d706%2Fdir_senti.gexf&sa=r&ca=d&st[]=r&st[]=t&st[]=t_&nr=0.701&lt=9.488"
-        frameBorder="0"
-        title="Retina"
-        allowFullScreen
-      ></iframe>
+    <iframe
+      width="800"
+      height="600"
+      src="https://ouestware.gitlab.io/retina/1.0.0-beta.1/#/embed/?url=https%3A%2F%2Fgist.githubusercontent.com%2Fchloeppd%2Fda85a845be35f7f8a69557542b244fe6%2Fraw%2F647f60617f209e4d51fc4c0d8a97145ebd750adb%2Fdir_senti.gexf&sa=r&ca[]=d&ca[]=t&st[]=r&st[]=t&nr=0.633&er=0.681&lt=5.929&ls=10"
+      frameBorder="0"
+      title="Retina"
+      allowFullScreen
+    ></iframe>
     </div>
   );
 };
