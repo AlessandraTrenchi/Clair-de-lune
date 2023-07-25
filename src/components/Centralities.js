98e5c43ac4e8dbb7c19ea0327d1ab45ca31766d4
Chloe <chloepap4@gmail.com>
2023-07-25 21:45:17 +0200

    network page-needs some edits

--- src/components/Centralities.js
+++ src/components/Centralities.js
@@ -2,15 +2,33 @@ import React from 'react';
 
 const Centralities = () => {
   return (
-    <div>
-      <h2>Centrality results</h2>
-      <p className="disco">
 
+<div className="textlu">
+<h2>Network Analysis</h2>
+<p className="disco">
+
+By applying centrality measures, we can also discover emotions that can have more critical roles in character interactions, story events, and the overall tone and atmosphere of the short stories;
+ These pivotal sentiments may be associated with key moments of tension, resolution, or emotional climax in the narratives.<br /><br />
+
+In-Degree centrality measures the number of incoming connections to a sentiment, which indicate how often the sentiment appears after other emotions and is influenced by them.
+In the context of our network, sentiments with a high in-degree are influential emotions that commonly appear after many other emotions,
+therefore having a central role in the emotional flow of the narrative.<br /><br />
+
+Out-Degree centrality, on the other hand, measures the number of outgoing emotional connections from a sentiment,
+indicating how frequently the sentiment is expressed and the extent to which it triggers other emotions.<br /><br />
+
+
+Betweenness Centrality quantifies the extent to which a sentiment acts as an intermediary between other emotions.
+Emotions that display a high betweenness centrality indicate they have potential control over the network, by influencing the
+relationships between sentiments, and connecting emotions that might not directly interact with each other.<br /><br />
+
+Lastly, Closeness Centrality measures the efficacy of a sentiment&apos;s influence in the network, by using the average shortest
+distance to all other sentiment pairs.<br /><br />
       <table>
       <thead>
         <tr>
           <th>Emotion</th>
-          <th>In degree centrality</th>
+          <th>In-Degree</th>
         </tr>
       </thead>
       <tbody>
@@ -56,14 +74,63 @@ const Centralities = () => {
         </tr>
       </tbody>
       </table>
-      </p>
 
-      <p className="disco">
+      <table>
+<thead>
+  <tr>
+    <th>Emotion</th>
+    <th>Out-Degree</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>fear</td>
+    <td>0.571429</td>
+  </tr>
+  <tr>
+    <td>surprise</td>
+    <td>0.514286</td>
+  </tr>
+  <tr>
+    <td>anger</td>
+    <td>0.485714</td>
+  </tr>
+  <tr>
+    <td>love</td>
+    <td>0.428571</td>
+  </tr>
+  <tr>
+    <td>sadness</td>
+    <td>0.428571</td>
+  </tr>
+  <tr>
+    <td>joy</td>
+    <td>0.400000</td>
+  </tr>
+  <tr>
+    <td>amazement</td>
+    <td>0.400000</td>
+  </tr>
+  <tr>
+    <td>anticipation</td>
+    <td>0.342857</td>
+  </tr>
+  <tr>
+    <td>despair</td>
+    <td>0.285714</td>
+  </tr>
+  <tr>
+    <td>remorse</td>
+    <td>0.285714</td>
+  </tr>
+</tbody>
+</table>
+
       <table>
         <thead>
           <tr>
             <th>Emotion</th>
-            <th>Betweenness centrality</th>
+            <th>Betweenness</th>
             <th></th>
           </tr>
         </thead>
@@ -104,11 +171,69 @@ const Centralities = () => {
             <td>despair</td>
             <td>0.042037</td>
           </tr>
+          <tr>
+            <td>trust</td>
+            <td>0.029848</td>
+          </tr>
+
+
         </tbody>
       </table>
-      </p>
 
-    </div>
+      <table>
+      <thead>
+        <tr>
+          <th>Emotion</th>
+          <th>Closeness</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>joy</td>
+          <td>0.610159</td>
+        </tr>
+        <tr>
+          <td>surprise</td>
+          <td>0.596894</td>
+        </tr>
+        <tr>
+          <td>fear</td>
+          <td>0.584195</td>
+        </tr>
+        <tr>
+          <td>sadness</td>
+          <td>0.528022</td>
+        </tr>
+        <tr>
+          <td>love</td>
+          <td>0.518059</td>
+        </tr>
+        <tr>
+          <td>anticipation</td>
+          <td>0.508466</td>
+        </tr>
+        <tr>
+          <td>amazement</td>
+          <td>0.508466</td>
+        </tr>
+        <tr>
+          <td>anger</td>
+          <td>0.499221</td>
+        </tr>
+        <tr>
+          <td>remorse</td>
+          <td>0.450117</td>
+        </tr>
+        <tr>
+          <td>serenity</td>
+          <td>0.442857</td>
+        </tr>
+      </tbody>
+      </table>
+</p>
+</div>
+
+
   );
 };
 
--- src/components/Network.js
+++ src/components/Network.js
@@ -19,9 +19,36 @@ const Network = () => {
         ></iframe>
 
       <div className="textlu">
+      <h2>Network</h2>
+
+      <p className="disco">
+
+      Graph is a means of visualization that enables a holistic view of sentiment connections within and across short stories.
+      By constructing a network of sentiments as nodes, and their relationships with directed edges, representing the flow and patterns of emotions
+      that emerge throughout Guy de Maupassant&apos;s collection. This broader perspective can allow us
+      to identify recurrent emotional motifs and discern common themes that could remain elusive through traditional reading methods;
+      indeed, in our network we intuitively notice....<br /><br />
+
+</p>
+
+</div>
 <Centralities />
 
-      </div>
+<div className="textlu">
+<h2>Further development</h2>
+
+<p className="disco">
+Furthermore, an alternative approach to reading implementing network theory can provide possibilities for a comparative study.
+With sentiment networks, one can observe and compare the use of emotional states between various authors and genres;
+it would be interesting, for example, to investigate if and how the historical context of a text or the personal life of the author can shape
+the way they use emotions in the narrative, or how relationship between emotional states can behave in different literary genres (e.g. psychological,
+  comedy, horror, etc).
+As Moretti advocates for comparing and contrasting literary data on a larger scale,
+you can use graph visualization to explore similarities and differences in emotional themes, character interactions,
+and narrative structures among various short stories. This comparative approach adds depth to your analysis and contributes
+ to a broader understanding of the emotional dimensions present in the short story genre.</p>
+ </div>
+
         <Footer />
       </div>
       </div>
