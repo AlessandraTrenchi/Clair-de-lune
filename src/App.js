cb6ee9227c3cd80d436c2eaec856b6cbf0f11f74
AlessandraTrenchi <alessandra.trenchi@studio.unibo.it>
2023-09-23 17:29:25 +0200

    installed dependencies

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Documentation from './components/Documentation';
 import Disclaimer from './components/Disclaimer';
 import Network from './components/Network';
 import Clair from './components/Clair';
+import { CodeBlock } from 'react-code-blocks';
+
 const App = () => {
   return (
     <Router>
--- src/components/Clair.js
+++ src/components/Clair.js
@@ -33,86 +33,33 @@ const Clair = () => {
 
   const [currentTitleIndex, setCurrentTitleIndex] = useState(0);
   const [currentXml, setCurrentXml] = useState('');
-  const [emozioni, setEmozioni] = useState([]);
 
-  useEffect(() => {
-    const currentTitle = textTitles[currentTitleIndex];
+  // Funzione per caricare l'XML in base all'indice del titolo
+  const loadXmlForCurrentTitle = () => {
     const currentXmlFile = textXmlFiles[currentTitleIndex];
-
+    const xmlFilePath = `src/text/${currentXmlFile}`;
+  
     // Utilizza CETEIcean per renderizzare il tuo XML
     const cetiOceanInstance = new CETEI();
-    cetiOceanInstance.getHTML5(`src/text/${currentXmlFile}`, function(data) {
+  
+    cetiOceanInstance.getHTML5(xmlFilePath, function(data) {
+      // Verifica se ci sono errori o avvisi
+      if (cetiOceanInstance.error) {
+        console.error('Errore durante il caricamento del documento XML:', cetiOceanInstance.error);
+      } else if (cetiOceanInstance.warning) {
+        console.warn('Avviso durante il caricamento del documento XML:', cetiOceanInstance.warning);
+      }
+  
+      // Imposta il documento XML corrente
       setCurrentXml(data);
-
-      // Estrai emozioni dal file XML e aggiungile a uno stato
-      const emozioniXML = extractEmozioniFromXML(data);
-      setEmozioni(emozioniXML);
-
-      // Evidenzia le emozioni nell'XML
-      highlightEmotionsInXML(emozioniXML);
     });
+  };
+
+  // Carica l'XML quando currentTitleIndex cambia
+  useEffect(() => {
+    loadXmlForCurrentTitle();
   }, [currentTitleIndex]);
 
-  const extractEmozioniFromXML = (xmlString) => {
-    // Crea un oggetto parser XML
-    const parser = new DOMParser();
-  
-    // Analizza il testo XML in un documento XML
-    const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
-  
-    // Trova tutti gli elementi 'interp' con attributo 'ana' (analogia)
-    const interpElements = xmlDoc.querySelectorAll('interp[ana]');
-  
-    // Estrai le emozioni dall'attributo 'ana' degli elementi 'interp'
-    const emozioni = Array.from(interpElements).map((element) => {
-      // Estrai il valore dell'attributo 'ana' (es. '#anger')
-      const anaValue = element.getAttribute('ana');
-  
-      // Rimuovi il carattere '#' se presente
-      return anaValue.replace('#', '');
-    });
-  
-    return emozioni;
-  };
-  
-
-  const highlightEmotionsInXML = (emozioniXML) => {
-    // Ottieni il contenuto HTML dell'XML corrente
-    const xmlContainer = document.querySelector('.xml-content');
-    const xmlContent = xmlContainer.querySelector('div');
-  
-    // Cicla attraverso gli elementi 'interp' e applica lo stile di evidenziazione alle emozioni
-    emozioniXML.forEach((emozione) => {
-      // Puoi definire uno stile CSS o un attributo per evidenziare le emozioni
-      // Ad esempio, aggiungi una classe CSS chiamata 'highlighted' o un attributo 'data-emotion'
-      const elementsToHighlight = xmlContent.querySelectorAll(`interp[ana="#${emozione}"]`);
-      
-      elementsToHighlight.forEach((element) => {
-        // Evidenzia l'elemento (ad esempio, applica una classe CSS o aggiungi un attributo)
-        element.classList.add('highlighted');
-        // Oppure, se preferisci aggiungere un attributo:
-        // element.setAttribute('data-emotion', emozione);
-      });
-    });
-  };
-  
-  const getCurrentEmotionIndex = (currentEmotion) => {
-    // Verifica se l'array delle emozioni è definito e non vuoto
-    if (emozioni && emozioni.length > 0) {
-      return emozioni.map((emozione, index) => (
-        <button
-          key={index}
-          className={index === getCurrentEmotionIndex(emozione) ? 'selected' : ''}
-        >
-          {emozione}
-        </button>
-      ));
-    }
-    
-    return null; // Restituisci null se l'array delle emozioni è vuoto o non definito
-  };
-  
-  
   const handleTitleChange = (titleIndex) => {
     setCurrentTitleIndex(titleIndex);
   };
@@ -141,7 +88,7 @@ const Clair = () => {
               <h2>{textTitles[currentTitleIndex]}</h2>
               <div dangerouslySetInnerHTML={{ __html: currentXml }} />
               <div className="menu-emozioni">
-                {getCurrentEmotionIndex()}
+                {/* Qui puoi mostrare le emozioni se lo desideri */}
               </div>
               <div className="pagination">
                 <button className="pp" onClick={handlePrev} disabled={currentTitleIndex === 0}>
