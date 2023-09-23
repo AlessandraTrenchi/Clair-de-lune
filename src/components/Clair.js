2fabd11611293f6627452730e151a25c0a1f87d5
AlessandraTrenchi <alessandra.trenchi@studio.unibo.it>
2023-09-23 14:51:01 +0200

    claire -> clair

--- src/components/Clair.js
+++ src/components/Clair.js
@@ -1,79 +1,69 @@
-import Footer from './Footer';
 import React, { useState, useEffect } from 'react';
-import XmlComponent from './XmlComponent';
-import Carousel from'./Carousel';
+import CETEI from 'CETEIcean';
 import '../index.css';
 
 const Clair = () => {
-  const images = [
-    { src: 'https://tse2.mm.bing.net/th?id=OIP.hlxUa6fXKdaqVuBLuNq4vQHaHa&pid=Api&P=0&h=180', xmlId: 'bijoux', className:'bijoux', title: 'Les Bijoux' },
-    { src: 'https://tse2.explicit.bing.net/th?id=OIP.DYpK6ZOo9s5VcwyaGg_mGAHaKh&pid=Api&P=0&h=180', xmlId: 'clair-de-lune', className:'bijoux', title: 'Clair de Lune' },
-    { src: ' https://tse4.mm.bing.net/th?id=OIP.CCexfoRMYzSZayzxyb1BvAHaH8&pid=Api&P=0&h=180', xmlId: 'le-loup', className:'bijoux', title: 'Le Loup' },
-    { src: 'https://tse2.mm.bing.net/th?id=OIP.nw7zC5s6Vc8cEAWGLYBX-gHaFv&pid=Api&P=0&h=180', xmlId: "coup-d-etat", className:'bijoux', title: "Un coup d'etat" },
-    { src: 'https://tse4.mm.bing.net/th?id=OIP.tNEBQlE-37Ss4zJ2wwGivAHaHa&pid=Api&P=0&h=180', xmlId: "l-enfant", className:'bijoux', title: "L'enfant" },
-    { src: 'https://tse4.mm.bing.net/th?id=OIP.uE_U5iEmjltNrkHc85u-tgHaKe&pid=Api&P=0&h=180', xmlId: "conte-de-noel", className:'bijoux', title: 'Conte de Noel' },
-    { src: 'https://tse1.mm.bing.net/th?id=OIP.uFSlYyB7TRlLAEFIpr5_UwHaIP&pid=Api&P=0&h=180', xmlId: "reine-hortense", className:'bijoux', title: 'La Reine Hortense' },
-    { src: 'https://tse2.mm.bing.net/th?id=OIP.K66ExGCP31eHF6fLttISDwHaFj&pid=Api&P=0&h=180', xmlId: "le-perdon", className:'bijoux', title: 'Le pardon' },
-    { src: 'https://tse2.mm.bing.net/th?id=OIP.t2edsMn7aFTIVav8TsZWBwHaEK&pid=Api&P=0&h=180', xmlId: "legende-mont-st-michel", className:'bijoux', title: 'La Legende du Mont Saint-Michel' },
-    { src: 'https://tse3.explicit.bing.net/th?id=OIP.6MNYlEeLqrv60hn445EPOQHaKy&pid=Api&P=0&h=180', xmlId: "une-veuve", className:'bijoux', title: 'Une veuve' },
-    { src: 'https://tse3.mm.bing.net/th?id=OIP.gX8fs1mLVA0BMnkQpCBm-QHaIr&pid=Api&P=0&h=180', xmlId: "mlle-cocotte", className:'bijoux', title: 'Mademoiselle Cocotte' },
-    { src: ' https://tse2.mm.bing.net/th?id=OIP.D9VmXZhVZ-gJ4DH6TWcPKwHaEK&pid=Api&P=0&h=180', xmlId: "apparition", className:'bijoux', title:'Apparition'}
+  const textTitles = [
+    'Les Bijoux',
+    'Clair de Lune',
+    // Aggiungi gli altri titoli qui
   ];
 
-  const [currentXmlId, setCurrentXmlId] = useState(null);
-  const [currentTextUrl, setCurrentTextUrl] = useState(null); // Define currentTextUrl
+  const [currentTitleIndex, setCurrentTitleIndex] = useState(0);
+  const [currentXml, setCurrentXml] = useState('');
 
-  const handleImageClick = (xmlId) => {
-    console.log('Selected XML ID:', xmlId);
-    const repo = 'https://raw.githubusercontent.com/AlessandraTrenchi/Clair-de-lune/main/src/text/';
-    setCurrentXmlId(xmlId);
+  useEffect(() => {
+    const currentTitle = textTitles[currentTitleIndex];
 
-    // Determine the corresponding texturl based on the selected xmlId
-    let texturl = '';
+    // Utilizza CETEIcean per renderizzare il tuo XML
+    const cetiOceanInstance = new CETEI();
+    cetiOceanInstance.getHTML5(`/text/${currentTitle}.xml`, function(data) {
+      setCurrentXml(data);
+    });
+  }, [currentTitleIndex]);
 
-    // Add logic to map xmlId to the corresponding texturl
-    // switch (xmlId) {
-    //   case 'les-bijoux':
-    //     texturl = 'https://raw.githubusercontent.com/AlessandraTrenchi/Clair-de-lune/main/src/text/bijoux.xml'; // Replace with the actual URL
-    //     break;
-    //
-    //   case 'clair-de-lune':
-    //     texturl = 'https://raw.githubusercontent.com/AlessandraTrenchi/Clair-de-lune/main/src/text/clair-de-lune.xml'; // Replace with the actual URL
-    //     break;
-    //
-    //   case 'le-loup':
-    //     texturl = 'https://raw.githubusercontent.com/AlessandraTrenchi/Clair-de-lune/main/src/text/le-loup.xml'; // Replace with the actual URL
-    //     break;
-    //   // Add more cases for other xmlIds as needed
-    //
-    //   default:
-    //     texturl = 'bijoux.xml'; // Set a default or handle the case where no URL is found
-    // }
-
-    setCurrentXmlId(xmlId);
-    setCurrentTextUrl(repo+xmlId+'.xml'); // Set the texturl state variable
+  const handleTitleChange = (titleIndex) => {
+    setCurrentTitleIndex(titleIndex);
   };
 
+  const handlePrev = () => {
+    if (currentTitleIndex > 0) {
+      setCurrentTitleIndex(currentTitleIndex - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentTitleIndex < textTitles.length - 1) {
+      setCurrentTitleIndex(currentTitleIndex + 1);
+    }
+  };
 
   return (
     <>
       <div className='nett'>
         <div className='carr'>
           <div className='clair-container'>
-          <h1>Clair de Lune Digital Edition</h1>
-
-          <h2>Select the text</h2>
-          <div className='centered-carousel'>
-            <Carousel images={images} onImageClick={handleImageClick} />
+            <h1>Clair de Lune Digital Edition</h1>
+          </div>
+          <div className='main-content'>
+            <div className="xml-content">
+  <h2>{textTitles[currentTitleIndex]}</h2>
+  <div dangerouslySetInnerHTML={{ __html: currentXml }} />
+  <div className="pagination">
+    <button className="pp" onClick={handlePrev} disabled={currentTitleIndex === 0}>
+      Precedente
+    </button>
+    <button className='pp'
+      onClick={handleNext}
+      disabled={currentTitleIndex === textTitles.length - 1}
+    >
+      Successivo
+    </button>
+  </div>
+</div>
           </div>
         </div>
-        <div className='main-content'>
-          {/* Render the XML content for the selected image */}
-
-          {currentXmlId && <XmlComponent texturl={currentTextUrl} />} {/* Pass the texturl prop */}
-        </div>
-      </div></div>
-      <Footer/>
+      </div>
     </>
   );
 };
