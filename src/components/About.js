c4ba2dd161c256b7bd4f7210d85bb94ee616ccf2
AlessandraTrenchi <alessandra.trenchi@studio.unibo.it>
2023-09-24 19:21:32 +0200

    noi

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,30 @@ const About = () => {
     <>
       <div className='cielo'>
         <div className='textlu'>
-          <h2>About</h2>
-          <p className='disco'>Welcome to our website!</p>
-          <p className='disco'>We are a company dedicated to providing high-quality products and services.</p>
-          <p className='disco'>Our mission is to create innovative solutions that meet our customers&apos; needs.</p>
-          <p className='disco'>Feel free to explore our website and learn more about what we do.</p>
+          <h2>About us</h2></div>
+          <div className='mom'><div>
+            <h2>Chloe Papadopoulou</h2><img src='https://media.licdn.com/dms/image/C4D03AQHRBfLRR53i0w/profile-displayphoto-shrink_800_800/0/1602861296568?e=1701302400&v=beta&t=72nqaZmVT5vegI1IUorGxAAxuS1PuhHeQr0rIMZKeXs' className='imm'></img>
+            <p className='noi'>I&lsquo;m Chloe Papadopoulou, a passionate web developer and a dedicated student pursuing my Master&lsquo;s in Digital Humanities and Digital Knowledge at the University of Bologna. My journey into the world of web development began with a profound curiosity about how the internet works and a desire to create meaningful digital experiences.
+
+With a strong foundation in HTML, CSS, and JavaScript, I have honed my skills in building responsive and user-friendly websites. My passion for clean and efficient code drives me to continuously explore new technologies and best practices in the ever-evolving field of web development.
+
+When I&lsquo;m not coding or immersed in my studies, you&lsquo;ll often find me exploring the intersections of technology and culture, seeking inspiration from the rich tapestry of the digital world. I believe that the web has the power to transform the way we connect and share information, and I&lsquo;m excited to be a part of that transformation.
+              </p></div>
+          
+         
+
+        </div>
+        <div className='mom'><div>
+            <h2>Alessandra Trenchi</h2><img src='https://ahsanv101.github.io/WebDev/images/AboutUs/ale.jpg' className='imm'></img>
+
+            <p className='noi'>Hello there! I&lsquo;m Alessandra Trenchi, a web developer and an enthusiast for all things digital. Currently, I&lsquo;m pursuing my Master&lsquo;s degree in Digital Humanities and Digital Knowledge at the esteemed University of Bologna. My journey into the world of web development began with a simple curiosity that has since grown into a deep passion.
+
+I thrive on the creative and problem-solving aspects of web development. Armed with a strong skill set that includes HTML, CSS, and JavaScript, I enjoy crafting websites that not only look great but also provide exceptional user experiences.
+
+Beyond coding, I&lsquo;m constantly inspired by the ways in which technology is reshaping our society and the possibilities it offers for innovative storytelling and communication. I believe that through our work in web development, we can contribute to shaping a more connected and digitally enriched world.
+              </p></div>
+          
+         
         </div>
       </div>
       <Footer />
