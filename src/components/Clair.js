19904fca98d16d3a22d148698dbc1c9a6a84d87b
Chloe <chloepap4@gmail.com>
2023-09-24 10:22:25 +0200

    edit img

--- src/components/Clair.js
+++ src/components/Clair.js
@@ -18,18 +18,18 @@ const emotionData = [
 const Clair = () => {
   const images = [
 
-    { src: 'src/img/click_img.png', xmlId: 'bijoux', className:'bijoux', title: 'Les Bijoux' },
-    { src: 'src/img/click_img.png', xmlId: 'clair-de-lune', className:'bijoux', title: 'Clair de Lune' },
-    { src: 'src/img/click_img.png', xmlId: 'le-loup', className:'bijoux', title: 'Le Loup' },
-    { src: 'src/img/click_img.png', xmlId: "coup-d-etat", className:'bijoux', title: "Un coup d'etat" },
-    { src: 'src/img/click_img.png', xmlId: "l-enfant", className:'bijoux', title: "L'enfant" },
-    { src: 'src/img/click_img.png', xmlId: "conte-de-noel", className:'bijoux', title: 'Conte de Noel' },
-    { src: 'src/img/click_img.png', xmlId: "reine-hortense", className:'bijoux', title: 'La Reine Hortense' },
-    { src: 'src/img/click_img.png', xmlId: "le-perdon", className:'bijoux', title: 'Le pardon' },
-    { src: 'src/img/click_img.png', xmlId: "legende-mont-st-michel", className:'bijoux', title: 'La Legende du Mont Saint-Michel' },
-    { src: 'src/img/click_img.png', xmlId: "une-veuve", className:'bijoux', title: 'Une veuve' },
-    { src: 'src/img/click_img.png', xmlId: "mlle-cocotte", className:'bijoux', title: 'Mademoiselle Cocotte' },
-    { src: 'src/img/click_img.png', xmlId: "apparition", className:'bijoux', title:'Apparition'}
+    { src: 'https://raw.githubusercontent.com/AlessandraTrenchi/Clair-de-lune/main/src/img/click_img.png', xmlId: 'bijoux', className:'bijoux', title: 'Les Bijoux' },
+    { src: 'https://raw.githubusercontent.com/AlessandraTrenchi/Clair-de-lune/main/src/img/click_img.png', xmlId: 'clair-de-lune', className:'bijoux', title: 'Clair de Lune' },
+    { src: 'https://raw.githubusercontent.com/AlessandraTrenchi/Clair-de-lune/main/src/img/click_img.png', xmlId: 'le-loup', className:'bijoux', title: 'Le Loup' },
+    { src: 'https://raw.githubusercontent.com/AlessandraTrenchi/Clair-de-lune/main/src/img/click_img.png', xmlId: "coup-d-etat", className:'bijoux', title: "Un coup d'etat" },
+    { src: 'https://raw.githubusercontent.com/AlessandraTrenchi/Clair-de-lune/main/src/img/click_img.png', xmlId: "l-enfant", className:'bijoux', title: "L'enfant" },
+    { src: 'https://raw.githubusercontent.com/AlessandraTrenchi/Clair-de-lune/main/src/img/click_img.png', xmlId: "conte-de-noel", className:'bijoux', title: 'Conte de Noel' },
+    { src: 'https://raw.githubusercontent.com/AlessandraTrenchi/Clair-de-lune/main/src/img/click_img.png', xmlId: "reine-hortense", className:'bijoux', title: 'La Reine Hortense' },
+    { src: 'https://raw.githubusercontent.com/AlessandraTrenchi/Clair-de-lune/main/src/img/click_img.png', xmlId: "le-perdon", className:'bijoux', title: 'Le pardon' },
+    { src: 'https://raw.githubusercontent.com/AlessandraTrenchi/Clair-de-lune/main/src/img/click_img.png', xmlId: "legende-mont-st-michel", className:'bijoux', title: 'La Legende du Mont Saint-Michel' },
+    { src: 'https://raw.githubusercontent.com/AlessandraTrenchi/Clair-de-lune/main/src/img/click_img.png', xmlId: "une-veuve", className:'bijoux', title: 'Une veuve' },
+    { src: 'https://raw.githubusercontent.com/AlessandraTrenchi/Clair-de-lune/main/src/img/click_img.png', xmlId: "mlle-cocotte", className:'bijoux', title: 'Mademoiselle Cocotte' },
+    { src: 'https://raw.githubusercontent.com/AlessandraTrenchi/Clair-de-lune/main/src/img/click_img.png', xmlId: "apparition", className:'bijoux', title:'Apparition'}
   ];
 
   const [currentXmlId, setCurrentXmlId] = useState(null);
@@ -61,8 +61,7 @@ const Clair = () => {
               <Carousel images={images} onImageClick={handleImageClick} />
             </div>
           </div>
-          <div className='emotions'> <h3>Emotions</h3>
-      <Emotions emotionData={emotionData} /></div>
+
           <div className="cont-tei">
           <div className='main-content'>
             {/* Render the XML content for the selected image */}
